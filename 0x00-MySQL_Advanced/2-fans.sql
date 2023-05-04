818ca36cfc6adbee2c53487e0721b887cc098c21
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-04 20:38:21 +0000

    MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+
+-- ranks country origins of bands, ordered by the number of (non-unique) fans:
+    -- Import this table dump: metal_bands.sql.zip
+    -- Column names must be: origin and nb_fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
