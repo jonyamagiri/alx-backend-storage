31da515033ebada8c554502635adcf360f8f054f
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-08 11:36:09 +0000

    MySQL_Advanced

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,22 +3,26 @@
     -- procedure ComputeAverageScoreForUser is taking 1 input
     -- user_id, a users.id value (you can assume user_id is linked to an existing users)
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DELIMITER ++
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
     DECLARE total_score INT DEFAULT 0;
     DECLARE projects_count INT DEFAULT 0;
 
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    SELECT COUNT(*) INTO projects_count
-    FROM corrections
-    WHERE user_id = user_id;
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
     UPDATE users
-    SET average_score = IFNULL(total_score/projects_count, 0)
-    WHERE id = user_id;
-END $$
+        SET users.average_score = CASE
+            WHEN projects_count = 0 THEN 0
+            ELSE total_score / projects_count
+            END
+        WHERE users.id = user_id;
+END ++
 DELIMITER ;
