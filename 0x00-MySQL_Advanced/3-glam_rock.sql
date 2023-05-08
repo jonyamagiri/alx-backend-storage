e00250087cf50b2edc550b4fece17dba5c717014
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-08 09:09:20 +0000

    MySQL_Advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,5 +4,5 @@
     -- use attributes formed and split for computing the lifespan
 SELECT band_name, IFNULL(split, 2020) - formed AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE COALESCE(style, '') LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
